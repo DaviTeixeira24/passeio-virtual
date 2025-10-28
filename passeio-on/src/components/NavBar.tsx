8c9b59353b4be8a5e088f77997452c517b791818
DaviTeixeira24 <davisilvateixeiramoreira@gmail.com>
2025-10-28 15:17:42 -0300

    Instalação de pacotes e configurações das dependencias instaladas

--- passeio-on/src/components/NavBar.tsx
+++ passeio-on/src/components/NavBar.tsx
@@ -1,26 +1,2 @@
-import React from "react";
-import { NavLink } from "react-router-dom";
 
-const NavBar = () => {
-  return (
-    <>
-      <Nav>
-        <NavLink to="/">PasseioVirtual</NavLink>
-        <Bars />
-        <NavMenu>
-          <NavLink to="/about" activeStyle>
-            Sobre
-          </NavLink>
-          <NavLink to="/contact" activeStyle>
-            Contato
-          </NavLink>
-          <NavLink to="/undefined" activeStyle>
-            Undefined
-          </NavLink>
-        </NavMenu>
-      </Nav>
-    </>
-  );
-};
 
-export default NavBar;
