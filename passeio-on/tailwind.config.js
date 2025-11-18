75647ef22f0397c0a40d9e815e3b0cbe4f14e1f2
DaviTeixeira24 <davisilvateixeiramoreira@gmail.com>
2025-11-18 17:04:17 -0300

    Criando animações e adicionando logos

--- passeio-on/tailwind.config.js
+++ passeio-on/tailwind.config.js
@@ -44,11 +44,15 @@ export default {
           "0%,100%": { boxShadow: "0 0 10px #1e57dcc4, 0 0 20px #275fe19d" },
           "50%": { boxShadow: "0 0 25px #1e57dc69, 0 0 50px #275fe15e" },
         },
+        border: {
+          to: { "--border-angle": "360deg" },
+        },
       },
       animation: {
         glow: "glow 3s ease-in-out infinite",
         glow2: "glow2 2.4s ease-in-out infinite",
         shimmer: "shimmer 1.3s linear infinite",
+        border: "border 4s linear infinite",
       },
     },
   },
