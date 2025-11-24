1f00bd3e5093e9e0a2e8ff6f6fa517ef6dd01c22
DaviTeixeira24 <davisilvateixeiramoreira@gmail.com>
2025-11-24 16:56:31 -0300

    componentizando efeito de nebula para o background

---
+++ passeio-on/src/components/Nebula.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Nebula() {
+  return (
+    <div className="pointer-events-none absolute inset-0 bg-[radial-gradient(circle_at_20%_20%,rgba(59,130,246,.18),transparent_40%),radial-gradient(circle_at_80%_30%,rgba(34,211,238,.15),transparent_35%)]" />
+  );
+}
+
+export default Nebula;
